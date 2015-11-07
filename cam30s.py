d93336153e228b659b3ed75b31ef5d6f451c5994
damballah <pougaud@gmail.com>
2015-11-07 19:15:30 +0100

    cam30s.py

--- cam30s.py
+++ cam30s.py
@@ -1,18 +1,18 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-# On importe les modules "GPIO" pour la communication, "picamera" pour gérer la cam, "time" pour faire des pause
+# On importe les modules "GPIO" pour la communication, "picamera" pour gérer le module cam et "time" pour faire des pauses
 import RPi.GPIO as GPIO
 import picamera
 import time
 
-# On configure les pin GPIO du Rpi en mode BCM (pour la notation)
+# On configure les pins GPIO du Rpi en mode BCM (pour la notation)
 GPIO.setmode(GPIO.BCM)
 
 # Nous lui indiquons que le signal electrique va sortir de la pin 25
 GPIO.setup(25,GPIO.OUT)
 
-# Le signal electrique est envoyé à la pin 25 afin d'allumer les LED infrarouge
+# Le signal electrique est envoyé à la pin 25 afin d'allumer les LEDS infrarouges
 GPIO.output(25,GPIO.HIGH)
 
 # Nous démarrons l'enregistrement de la vidéo pendant 30 secondes puis nous l'arrêtons.
@@ -21,5 +21,5 @@ with picamera.PiCamera() as camera:
     camera.start_recording('foo.h264')
     time.sleep(30)   
     camera.stop_recording()
-# Nous coupons le signal electrique envoyé à la pin 25, les LED s'éteignent    
+# Nous coupons le signal electrique envoyé à la pin 25, les LEDS s'éteignent    
     GPIO.output(25,GPIO.LOW)
